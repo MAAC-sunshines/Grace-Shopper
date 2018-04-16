c05eb847dc4b795462dcc7e26c177c24f4bf19bc
Alexa Moy <s.alexa.moy@gmail.com>
2018-04-16 16:53:21 -0400

    reducers set up

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,14 @@ import createLogger from 'redux-logger'
 import thunkMiddleware from 'redux-thunk'
 import {composeWithDevTools} from 'redux-devtools-extension'
 import user from './user'
+import instruments from '../reducers/instruments';
+import selectedInstrument from '../reducers/selectedInstrument';
 
-const reducer = combineReducers({user})
+const reducer = combineReducers({
+  user,
+  instruments,
+  selectedInstrument
+})
 const middleware = composeWithDevTools(applyMiddleware(
   thunkMiddleware,
   createLogger({collapsed: true})
