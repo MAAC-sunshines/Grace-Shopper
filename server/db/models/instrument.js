297e97dc16da3898a4ad8b9c2cb74d9c1934da64
Alla Yakubova <yakubova92@gmail.com>
2018-04-16 16:36:58 -0400

    added instrument model

---
+++ server/db/models/instrument.js
@@ -0,0 +1,33 @@
+//const crypto = require('crypto')
+const Sequelize = require('sequelize');
+const db = require('../db');
+
+const Instrument = db.define('instrument', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  type: {
+    type: Sequelize.STRING,
+    allowNull: false
+  },
+  imageURL: {
+    type: Sequelize.TEXT,
+    defaultValue: 'http://airguitarcanada.org/wp-content/uploads/2017/07/AGC-Championships-15July2017-605-1024x684.jpg'
+  },
+  cost: {
+    type: Sequelize.DECIMAL,
+    allowNull: false
+  },
+  category: {
+    type: Sequelize.ENUM,
+    values: ['string', 'woodwind', 'percussion', 'brass', 'keyboard'],
+    allowNull: false
+  },
+  description: {
+    type: Sequelize.TEXT
+  }
+});
+
+module.exports = Instrument;
+
