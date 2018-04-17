910e7eb778ee90c42ec9d51402b5b4ffdae9e2cd
Alexa Moy <s.alexa.moy@gmail.com>
2018-04-17 16:23:46 -0400

    single category

--- client/components/SingleCategory.js
+++ client/components/SingleCategory.js
@@ -9,9 +9,11 @@ export default class SingleCategory extends Component {
 
   render() {
     const {instruments = []} = this.props;
+    const instrument = instruments[0];
+    const category = instrument && instrument.categoryId;
     return (
       <div>
-        <h2>CATEGORIES</h2>
+        <h2>CATEGORY</h2>
         <div>
           {
             instruments && instruments.map(singleInstrument => {
@@ -20,9 +22,10 @@ export default class SingleCategory extends Component {
                   <h2>{singleInstrument.name}</h2>
                   <Link to={`/instruments/${singleInstrument.id}`}>
                   <img src={singleInstrument.imageUrl} />
+                  <h6>***Case not included***</h6>
                   </Link>
                   <h4>{singleInstrument.description}</h4>
-                  <h6>${singleInstrument.cost}</h6>
+                  <h5>${singleInstrument.cost}</h5>
                   </div>
                 )
             })
