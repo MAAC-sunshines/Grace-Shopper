b4703f0af6dfaf34a238a8d16ddf8a45521faac8
Ashi Krishnan <me@ashi.works>
2018-04-18 11:54:03 -0400

    Use defaultValue rather than placeholder so we can edit the current values.

--- client/components/UpdateInstrument.js
+++ client/components/UpdateInstrument.js
@@ -8,26 +8,26 @@ export default function InstrumentForm(props){
             <form onSubmit={props.handleSubmit}>
                 <h2>
                     <label>Instrument Name: </label>
-                    <input type="text" name="name" placeholder={instrument.name} />
+                    <input type="text" name="name" defaultValue={instrument.name} />
                 </h2>
                 <img src={instrument.imageUrl} />
                 <h3>
                     <label>Image url: </label>
-                    <input type="text" name="imageUrl" placeholder={instrument.imageUrl} />
+                    <input type="text" name="imageUrl" defaultValue={instrument.imageUrl} />
                 </h3>
                 <h3>
                     <label>Price: </label>
-                    <input type="number" name="cost" placeholder={instrument.cost} />
+                    <input type="number" name="cost" defaultValue={instrument.cost} />
                 </h3>
                 <h4>
                     <label>Category: </label>
-                    <input type="text" name="categoryId" placeholder={instrument.categoryId} />
+                    <input type="text" name="categoryId" defaultValue={instrument.categoryId} />
                 </h4>
                 <h4>
                     <label>Description: </label>
                 </h4>
                 <p>
-                    <textarea type="text" name="description" placeholder={instrument.description} />
+                    <textarea type="text" name="description" defaultValue={instrument.description} />
                 </p>
                 <button type="submit">Update</button>
             </form>
