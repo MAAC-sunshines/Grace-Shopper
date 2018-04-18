3af3e8da67ed242bcb804420a96d6cb11c65900c
Alla Yakubova <yakubova92@gmail.com>
2018-04-18 16:32:30 -0400

    allInstruments component has search functionality

--- client/components/AllInstruments.js
+++ client/components/AllInstruments.js
@@ -4,47 +4,41 @@ import { Button, Image } from 'react-bootstrap';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
+import Instruments from './Instruments';
 
 export default class AllInstruments extends Component {
-  constructor() {
-    super();
-    this.onClickHandler = this.onClickHandler.bind(this);
-  }
+  state = { query: '' }
 
   componentDidMount() {
     this.props.loadAllInstruments();
   }
 
-  onClickHandler() {
-    //render the findOrCreate form from Alexa
+  handleChange = (event) => {
+    this.setState ({
+      query: event.target.value.toLocaleLowerCase()
+    })
+  }
+
+  get instruments() {
+    const {allInstruments=[]} = this.props
+    return allInstruments
+      .filter(instrument => instrument.name
+        .toLocaleLowerCase()
+        .indexOf(this.state.query) !== -1)
   }
 
   render() {
-    const allInstruments = this.props.allInstruments;
     return (
-      <Grid className="all-categories-box">
-        <div className="subheader">
-          <h2>All Instruments</h2>
-          <Button bsStyle="primary" bsSize="xsmall">ADD NEW INSTRUMENT</Button>
-        </div>
-        <Row className="row-mapping">
-          {
-            allInstruments && allInstruments.map(single => {
-              return (
-                <Col sm={3} key={single.id} className="category-box">
-                  <Link to={`/instruments/${single.id}`}>
-                  <Image src={single.imageUrl} rounded className="thumbnail-photo"/>
-                    <li>
-                      <h2>{single.name}</h2>
-                      <p>Price: ${single.cost}</p>
-                    </li>
-                    </Link>
-                </Col>
-              )
-            })
-          }
-      </Row>
-      </Grid>
+      <div className="subheader">
+        <h2>All Instruments</h2>
+        <input
+          placeholder = "Enter instrument name"
+          onChange={this.handleChange}
+        />
+        <Instruments instruments={this.instruments} />
+        <Button bsStyle="primary" bsSize="xsmall">ADD NEW INSTRUMENT</Button>
+      </div>
     )
   }
 }
+
