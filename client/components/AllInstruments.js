7c86e153ca7e7ced1152727dd8931d539e4de0c3
Mahiyaa <mmahia1121@gmail.com>
2018-04-18 12:01:32 -0400

    AHAHAHAHAH

--- client/components/AllInstruments.js
+++ client/components/AllInstruments.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
+import store from '../store';
 
 export default class AllInstruments extends Component {
   constructor() {
     super();
     this.onClickHandler = this.onClickHandler.bind(this);
+    this.state = store.getState()
+    this.handleChange = this.handleChange.bind(this)
   }
 
   componentDidMount() {
@@ -15,15 +18,31 @@ export default class AllInstruments extends Component {
     //render the findOrCreate form
   }
 
+  handleChange(event){
+    this.setState({
+      inputValue: event.target.value
+    })
+  }
+
   render() {
     const allInstruments = this.props.allInstruments;
+    let dirty = false;
+    if (this.state.searchInput.length > 1) dirty = true
+    const instruments = dirty && allInstruments.filter(instrument => instrument.name.match(this.state.inputValue))
     return (
       <div>
         <h2>All Instruments</h2>
+        <h2>HIIIIII</h2>
+        <form>
+          <input
+            placeholder = "Enter instrument name"
+            onChange={this.handleChange}
+          />
+        </form>
         <button>ADD NEW INSTRUMENT</button>
         <ul>
           {
-            allInstruments && allInstruments.map(single => {
+            instruments && instruments.map(single => {
               return (
               <div key={single.id}>
                 <Link to={`/instruments/${single.id}`}>
