53f0c2555023f5fb049c8b02632574d712cecefa
Alexa Moy <s.alexa.moy@gmail.com>
2018-04-17 20:04:42 -0400

    PUT FINALLY WORKS HALLELUJAH

--- client/containers/SingleInstrumentContainer.js
+++ client/containers/SingleInstrumentContainer.js
@@ -1,6 +1,8 @@
-import { fetchInstrument, deleteInstrument, getInstrument } from '../store';
+import { fetchInstrument, deleteInstrument, getInstrument, putInstrument } from '../store';
+import React from 'react';
 import { connect } from 'react-redux';
 import SingleInstrument from '../components/SingleInstrument';
+import UpdateInstrument from '../components/UpdateInstrument';
 
 const mapStateToProps = state => {
     return {
@@ -10,15 +12,41 @@ const mapStateToProps = state => {
 
 const mapDispatchToProps = (dispatch, ownProps) => {
     return {
-        loadInstrument: function(){
+        loadInstrument: function () {
             const instrumentId = ownProps.match.params.id;
             return dispatch(fetchInstrument(instrumentId));
         },
-        handleDelete(event, id){
-			event.preventDefault();
-			dispatch(deleteInstrument(id, ownProps.history));
-			dispatch(getInstrument(''));
-		}
+        handleDelete(event, id) {
+            event.preventDefault();
+            dispatch(deleteInstrument(id, ownProps.history));
+            dispatch(getInstrument(''));
+        },
+        handleClick(event) {
+            event.preventDefault();
+            return (
+                <UpdateInstrument />
+            )
+        },
+        handleSubmit(instrument, event) {
+            event.preventDefault();
+            if (event.target.name.value){
+                instrument.name = event.target.name.value;
+            }
+            if (event.target.imageUrl.value){
+                instrument.imageUrl = event.target.imageUrl.value;
+            }
+            if (event.target.cost.value){
+                instrument.cost = +event.target.cost.value;
+            }
+            if (event.target.categoryId.value){
+                instrument.categoryId = event.target.categoryId.value;
+            }
+            if (event.target.description.value){
+                instrument.description = event.target.description.value;
+            }
+            dispatch(putInstrument(instrument, ownProps.history));
+            dispatch(getInstrument(''));
+        }
     };
 };
 
