99edd1cf299abece18801979de178b8dabff97f6
Alexa Moy <s.alexa.moy@gmail.com>
2018-04-18 11:32:22 -0400

    fixed form

--- client/components/SingleInstrument.js
+++ client/components/SingleInstrument.js
@@ -4,7 +4,7 @@ import { Button, Image } from 'react-bootstrap';
 import Grid from 'react-bootstrap/lib/Grid';
 import Row from 'react-bootstrap/lib/Row';
 import Col from 'react-bootstrap/lib/Col';
-
+import UpdateInstrument from './UpdateInstrument';
 export default class SingleProduct extends Component {
     constructor(props){
         super(props);
