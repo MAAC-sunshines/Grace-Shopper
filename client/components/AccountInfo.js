7acbf5dd60a6e28816e8ac363166ac4a75070891
Alexa Moy <s.alexa.moy@gmail.com>
2018-04-21 18:35:11 -0400

    more stuff

--- client/components/AccountInfo.js
+++ client/components/AccountInfo.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Grid from 'react-bootstrap/lib/Grid';
 
-export default function AccountInfo(props){
+export function AccountInfo(props) {
+    const { email } = props
+    const { firstName } = props || email
     return (
-        <div>
-            <h2>Edit </h2>
-        </div>
+        <Grid className="all-categories-box">
+            <div className="subheader">
+                <h2>Edit Shit</h2>
+            </div>
+        </Grid>
     )
+}
+
+const mapState = (state) => {
+    return {
+        email: state.user.email,
+        firstName: state.user.firstName
+    }
+}
+export default connect(mapState)(AccountInfo);
+
+AccountInfo.propTypes = {
+    email: PropTypes.string,
+    firstName: PropTypes.string
 }
