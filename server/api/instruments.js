76de7e447a825e6a4fa6e8938abbf1902cdf0508
Alexa Moy <s.alexa.moy@gmail.com>
2018-04-24 14:04:12 -0400

    backend authorization not including order-history

--- server/api/instruments.js
+++ server/api/instruments.js
@@ -2,6 +2,16 @@ const router = require('express').Router()
 const { Instrument } = require('../db/models')
 module.exports = router
 
+function throwError(status, msg) {
+  const err = new Error(msg);
+  err.status = status;
+  throw err;
+}
+function isAdmin (req, res, next) {
+  if (!req.user.admin) throwError(401, 'Unauthorized')
+  next()
+}
+
 //route to get ALL INSTRUMENTS on single page
 router.get('/', (req, res, next) => {
   Instrument.findAll()
@@ -24,7 +34,7 @@ router.get('/:id', (req, res, next) => {
 })
 
 //add a new instrument
-router.post('/', (req, res, next) => {
+router.post('/', isAdmin, (req, res, next) => {
   Instrument.create(req.body)
     .then(instrument => {
       res.json(instrument)
@@ -34,7 +44,7 @@ router.post('/', (req, res, next) => {
 
 //delete an instrument
 
-router.delete('/:id', (req, res, next) => {
+router.delete('/:id', isAdmin, (req, res, next) => {
   Instrument.destroy({
     where: {
       id: req.params.id
@@ -46,7 +56,7 @@ router.delete('/:id', (req, res, next) => {
   .catch(next)
 })
 
-router.put('/:id', (req, res, next) => {
+router.put('/:id', isAdmin, (req, res, next) => {
   const instrumentId = req.params.id;
   Instrument.findById(instrumentId)
         .then(instrument => instrument.update(req.body))
